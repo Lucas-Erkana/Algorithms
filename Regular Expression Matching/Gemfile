3b79e8e23803281cfc3b46fd46542334f81a4e6a
Lucas David Erkana <lderkana21x@gmail.com>
2023-08-09 13:48:53 +0200

    Solution

--- Regular Expression Matching/Gemfile
+++
@@ -1,2 +0,0 @@
-source 'https://rubygems.org'
-gem 'rubocop', '>= 1.0', '< 2.0'
--- Two sum/solution.rb
+++ Two sum/solution.rb
@@ -2,16 +2,30 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-      #write your code here 
-end
-      
-      nums = [2, 7, 11, 15]
-      target = 9
-      
-      # Call the two_sum function
-      result = two_sum(nums, target)
-      
-      # Example output
-      puts result.inspect   
+        # Create a hash to store the index of each number in nums
+        num_index = {}
+        
+        # Iterate through nums
+        nums.each_with_index do |num, index|
+          # Check if the complement of num exists in num_index
+          complement = target - num
+          if num_index.key?(complement)
+            # If complement exists, return the indices of num and complement
+            return [num_index[complement], index]
+          end
+          
+          # Add num and its index to num_index
+          num_index[num] = index
+        end
+        
+        # If no solution is found, return an empty array
+        return []
+      end
 
-      #expected result [0, 1]
+nums = [2, 7, 11, 15]
+target = 9
+# Call the two_sum function
+result = two_sum(nums, target)
+
+# Example output
+puts result.inspect   # [0, 1]
