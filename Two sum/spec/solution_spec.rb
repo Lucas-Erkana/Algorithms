4be191cd5798da6c421f77edfd15d19738a16cd2
Lucas David Erkana <lderkana21x@gmail.com>
2023-08-09 13:47:37 +0200

    Update solution_spec.rb

--- Two sum/spec/solution_spec.rb
+++ Two sum/spec/solution_spec.rb
@@ -7,10 +7,23 @@ describe "#two_sum" do
     target = 9
     expect(two_sum(nums, target)).to eq([0, 1])
     
+  end
+end
+
+describe "#two_sum" do
+  it "returns the indices of two numbers that add up to target" do
+    nums = [2, 1, 13, 1]
+    target = 4
+    expect(two_sum(nums, target)).to eq([])
+    
     nums = [3, 2, 4]
     target = 6
     expect(two_sum(nums, target)).to eq([1, 2])
-    
+  end
+end
+describe "#two_sum" do
+  it "returns the indices of two numbers that add up to target" do
+  
     nums = [3, 3]
     target = 6
     expect(two_sum(nums, target)).to eq([0, 1])
