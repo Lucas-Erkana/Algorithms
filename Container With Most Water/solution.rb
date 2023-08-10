45a22ca8315059649adfc797806804aa0f592922
Lucas David Erkana <lderkana21x@gmail.com>
2023-08-10 15:46:53 +0200

    Solution to Container With Most Water

--- Container With Most Water/solution.rb
+++ Container With Most Water/solution.rb
@@ -1,5 +1,39 @@
 # @param {Integer[]} height
 # @return {Integer}
 def max_area(height)
-
+    max_area = 0
+  left = 0
+  right = height.length - 1
+  
+  while left < right
+      max_area = [max_area, [height[left], height[right]].min * (right - left)].max
+      if height[left] < height[right]
+          left += 1
+      else
+          right -= 1
+      end
+  end
+  
+  max_area  
 end
+
+height = [1,8,6,2,5,4,8,3,7]
+result = max_area(height)
+puts result # expected result is 49
+
+height = [1,1]
+result = max_area(height)
+puts result # expected result is 1
+
+
+height = [3,1,4,5,2,6]
+result = max_area(height)
+puts result # expected result is 15
+
+height = [7,3,2,9,1]
+result = max_area(height)
+puts result # expected result is 21
+
+height = [10,2,8,9,5,7,6]
+result = max_area(height)
+puts result # expected result is 36
--- Container With Most Water/spec/solution_spec.rb
+++ Container With Most Water/spec/solution_spec.rb
@@ -1,31 +1,34 @@
 require_relative '../solution'
 require 'rspec'
 
-describe "#two_sum" do
-  it "returns the indices of two numbers that add up to target" do
-    nums = [2, 7, 11, 15]
-    target = 9
-    expect(two_sum(nums, target)).to eq([0, 1])
-    
+describe '#max_area' do
+  it 'calculates the max area for the given heights' do
+    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    result = max_area(height)
+    expect(result).to eq(49)
   end
-end
 
-describe "#two_sum" do
-  it "returns the indices of two numbers that add up to target" do
-    nums = [2, 1, 13, 1]
-    target = 4
-    expect(two_sum(nums, target)).to eq([])
-    
-    nums = [3, 2, 4]
-    target = 6
-    expect(two_sum(nums, target)).to eq([1, 2])
+  it 'calculates the max area for a simple case' do
+    height = [1, 1]
+    result = max_area(height)
+    expect(result).to eq(1)
   end
-end
-describe "#two_sum" do
-  it "returns the indices of two numbers that add up to target" do
-  
-    nums = [3, 3]
-    target = 6
-    expect(two_sum(nums, target)).to eq([0, 1])
+
+  it 'calculates the max area for another case' do
+    height = [3, 1, 4, 5, 2, 6]
+    result = max_area(height)
+    expect(result).to eq(15)
+  end
+
+  it 'calculates the max area for a different configuration' do
+    height = [7, 3, 2, 9, 1]
+    result = max_area(height)
+    expect(result).to eq(21)
+  end
+
+  it 'calculates the max area for a more complex case' do
+    height = [10, 2, 8, 9, 5, 7, 6]
+    result = max_area(height)
+    expect(result).to eq(36)
   end
 end
