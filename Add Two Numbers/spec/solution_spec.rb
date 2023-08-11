2463d52cfc8d3e840151d8fc3afcb84fc1db5655
Lucas David Erkana <lderkana21x@gmail.com>
2023-08-11 18:35:08 +0200

    Update solution_spec.rb

--- Add Two Numbers/spec/solution_spec.rb
+++ Add Two Numbers/spec/solution_spec.rb
@@ -1,66 +1,47 @@
 require_relative '../solution'
 require 'rspec'
 
-describe 'remove_nth_from_end' do
-  context 'when given a linked list and n' do
-    it 'removes the nth node from the end of the linked list for Test Case 1' do
-      # Test Case 1
-      head = [1, 2, 3, 4, 5]
-      n = 2
-      # Convert the array to a ListNode object
-      head = array_to_listnode(head)
-      result = remove_nth_from_end(head, n)
-      # Convert the ListNode object back to an array
-      result_array = listnode_to_array(result)
-      expect(result_array).to eq([1, 2, 3, 5])
+RSpec.describe 'add_two_numbers' do
+  context 'when given two linked lists' do
+    it 'returns the sum of the linked lists (Test Case 1)' do
+      # Inputs
+      l1 = ListNode.new(2, ListNode.new(4, ListNode.new(3)))
+      l2 = ListNode.new(5, ListNode.new(6, ListNode.new(4)))
+      # Output
+      expect(add_two_numbers(l1, l2)).to eq([7, 0, 8])
     end
-
-    it 'removes the nth node from the end of the linked list for Test Case 2' do
-      # Test Case 2
-      head = [1]
-      n = 1
-      # Convert the array to a ListNode object
-      head = array_to_listnode(head)
-      result = remove_nth_from_end(head, n)
-      # Convert the ListNode object back to an array
-      result_array = listnode_to_array(result)
-      expect(result_array).to eq([])
+  
+    it 'returns the sum of the linked lists (Test Case 2)' do
+      # Inputs
+      l1 = ListNode.new(0)
+      l2 = ListNode.new(0)
+      # Output
+      expect(add_two_numbers(l1, l2)).to eq([0])
     end
-
-    it 'removes the nth node from the end of the linked list for Test Case 3' do
-      # Test Case 3
-      head = [1, 2]
-      n = 1
-      # Convert the array to a ListNode object
-      head = array_to_listnode(head)
-      result = remove_nth_from_end(head, n)
-      # Convert the ListNode object back to an array
-      result_array = listnode_to_array(result)
-      expect(result_array).to eq([1])
+    
+    it 'returns the sum of the linked lists (Test Case 3)' do
+      # Inputs
+      l1 = ListNode.new(9, ListNode.new(9, ListNode.new(9, ListNode.new(9, ListNode.new(9, ListNode.new(9, ListNode.new(9)))))))
+      l2 = ListNode.new(9, ListNode.new(9, ListNode.new(9, ListNode.new(9))))
+      # Output
+      expect(add_two_numbers(l1, l2)).to eq([8, 9, 9, 9, 0, 0, 0, 1])
     end
-
-    it 'removes the nth node from the end of the linked list for Test Case 4' do
-      # Test Case 4
-      head = [1, 2]
-      n = 2
-      # Convert the array to a ListNode object
-      head = array_to_listnode(head)
-      result = remove_nth_from_end(head, n)
-      # Convert the ListNode object back to an array
-      result_array = listnode_to_array(result)
-      expect(result_array).to eq([2])
+    
+    it 'returns the sum of the linked lists (Test Case 4)' do
+      # Inputs
+      l1 = ListNode.new(9, ListNode.new(9, ListNode.new(9, ListNode.new(9, ListNode.new(9, ListNode.new(9, ListNode.new(9)))))))
+      l2 = ListNode.new(9, ListNode.new(9, ListNode.new(9, ListNode.new(9))))
+      # Output
+      expect(add_two_numbers(l1, l2)).to eq([8, 9, 9, 9, 0, 0, 0, 1])
     end
-
-    it 'removes the nth node from the end of the linked list for Test Case 5' do
-      # Test Case 5
-      head = [1, 2, 3, 4, 5]
-      n = 5
-      # Convert the array to a ListNode object
-      head = array_to_listnode(head)
-      result = remove_nth_from_end(head, n)
-      # Convert the ListNode object back to an array
-      result_array = listnode_to_array(result)
-      expect(result_array).to eq([2, 3, 4, 5])
+    
+    it 'returns the sum of the linked lists (Test Case 5)' do
+      # Inputs
+      l1 = ListNode.new(0)
+      l2 = ListNode.new(0)
+      
+      # Output
+      expect(add_two_numbers(l1, l2)).to eq([0])
     end
   end
 end
